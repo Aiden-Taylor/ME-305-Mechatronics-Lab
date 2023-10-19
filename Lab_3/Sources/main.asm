8c309e068192ffa3d5c7db88f9a970b195a91463
Aiden-Taylor <57740540+Aiden-Taylor@users.noreply.github.com>
2023-10-19 15:04:26 -0700

    Add files via upload

--- Lab_3/Sources/main.asm
+++ Lab_3/Sources/main.asm
@@ -1,12 +1,12 @@
 ;**************************************************************************************
-;* Blank Project Main [includes LibV2.2]                                              *
+;* Lab 3 Main [includes LibV2.2]                                              *
 ;**************************************************************************************
 ;* Summary:                                                                           *
 ;*   -                                                                                *
 ;*                                                                                    *
-;* Author: YOUR NAME                                                                  *
+;* Author: Aiden Taylor & Julia Fay                                                   *
 ;*   Cal Poly University                                                              *
-;*   Spring 2022                                                                      *
+;*   Fall 2023                                                                      *
 ;*                                                                                    *
 ;* Revision History:                                                                  *
 ;*   -                                                                                *
@@ -59,9 +59,32 @@
 
 DEFAULT_RAM:  SECTION
 
+
+
+;params for t2 
+KEY_FLG DS.B 1
+KEY_BUFF DS.W 1
+
+;params for t3
+MSG_NUM DS.B 1
+
+;state vars
+t1state DS.B 1
+t2state DS.B 1
+t3state DS.B 1
+t4state DS.B 1
+t5state DS.B 1
+t6state DS.B 1
+t7state DS.B 1
+t8state DS.B 1
+
+;subroutines
+RESULT DS.W 1
 COUNT DS.B 1 
 BUFFER DS.B 5 
-INPUT DS.B 1 
+INPUT DS.B 1
+DPTR DS.W 1
+FIRSTCH DS.B 1
 
 ;/------------------------------------------------------------------------------------\
 ;|  Main Program Code                                                                 |
@@ -69,71 +92,239 @@ INPUT DS.B 1
 ; Your code goes here
 
 MyCode:       SECTION
-main:   
+main:  
+       clr t1state ; initialize all tasks to state0
+       clr t2state
+       clr t3state
+       clr t4state
+       clr t5state
+       clr t6state
+       clr t7state
+       clr t8state
        
-        
-       jsr INITLCD       ;initialize LCD 
-       ldaa #$00         ;set LCD position to 0
-       ldx #MESSAGE      ;load message into x 
-       jsr OUTSTRING     ;display message 
-       jsr INITKEY       ;initialize keypad 
-       jsr CURSOR_ON     ;turn on cursor 
        
        
-      ;create loop to load buffer with inputted variables 
-      
-       clr COUNT 
-       ldy #BUFFER
-       
- buffer_loop: 
+spin: bra spin
  
-       ldaa COUNT 
-       cmpa #$05         ;accumulator a - $5    check if input is a number 
-       beq spin          ;if count is 5 then we are done and exit the loop 
-       
-       
-       jsr GETCHAR       ;load accumulator b with the keypad input 
-       cmpb #$08         ;accumulator b - $08    check if input is a backspace
-       beq backspace 
-       
-       
-       
-       cmpb #$39         ;accumulator b - $39    check if input is a number 
-       bgt buffer_loop   ;if input is not a number, go to the top of the loop 
-       
-       stab a, y         ;load the contents of accumulator b into buffer 
-       jsr OUTCHAR       ;display the input charater on the LCD 
-       
-       inc COUNT         ;increment count 
-                      ;increment x 
-       
-       bra buffer_loop    
-       
-       
- backspace: 
-       jsr GETADDR                   ;get current position of LCR 
-       deca                          ;decrement one 
-       jsr SETADDR                   ;set address to new position 
-       ldx #BACKSPACE               ;
-       jsr OUTSTRING                 ;output a black character 
-       jsr GETADDR                   ;get current position of LCR 
-       deca                          ;decrement one 
-       jsr SETADDR
-       dec COUNT 
-      
-      
-            
-      bra buffer_loop 
+;----------------------TASK 2-------------------------------------------; 
+ 
+TASK_2:
  
        
-      
- spin: bra spin; 
- 
+        ldaa t2state ;get state
+        beq t2s0
+        deca
+        beq t2s1
+        deca
+        beq t2s2
+        rts
 
+
+t2s0:
+
+        ;init
+        jsr INITKEY       ;initialize keypad
+        
+        movb #$01, t2state
+        rts
+        
+t2s1:     
+   
+        tst LKEY_FLG
+        beq exit2
+        jsr GETCHAR
+        stab #KEY_BUFF     ;stores the input char into key buffer
+        set KEY_FLG        ;notifies MM of key input
+        movb #$02, t2state
+        rts
+        
+t2s2:
+
+        tst KEY_FLG
+        bne exit2
+        movb #$01, t2state
+        
+
+exit2: rts        
+        
+        
+;---------------------TASK 3------------------------------------------------;
+
+TASK_3
+
+        ldaa t3state
+        beq t3s0
+        deca
+        beq t3s1
+        deca
+        beq t3s2
+        deca
+        beq t3s3
+        rts
+        
+t3s0:         
+        
+       jsr INITLCD       ;initialize LCD
+       ldaa #$00         ;set LCD position to 0
+       ldx #MESSAGE      ;load message into x
+       jsr OUTSTRING     ;display message
+       jsr CURSOR_ON     ;turn on cursor
+       movb #$01, t3state
+       rts
+        
+t3s1:
+
+       ;hub
+       set FIRSTCH
+       ldab MSG_NUM
+       stab t3state
+       rts
+
+
+t3s2:
+
+
+        jsr backspace
+        rts
+        
+
+
+t3s3:        
+        
+        ;make one state per msg
+        ;set dptr to #msg then jsr putchar1 if firstch and putchar if not firstch
+        
+        
+        
+        
+  
 ;/------------------------------------------------------------------------------------\
 ;| Subroutines                                                                        |
 ;\------------------------------------------------------------------------------------/
 ; General purpose subroutines go here
+
+  
+ ;---------------------------------------------------------------------------------------     
+
+input:
+       
+;create loop to load buffer with inputted variables
+     
+       clr COUNT
+       ldy #BUFFER
+       
+  input_loop:
+ 
+       ldaa COUNT              ;load accumulator a with the value of COUNT
+       cmpa #$05               ;accumulator a - $5    check if COUNT is 5
+       beq input_exit          ;if count is 5 then we are done and exit the loop
+       
+       
+       jsr GETCHAR             ;load accumulator b with the keypad input
+       cmpb #$08               ;accumulator b - $08    check if input is a backspace
+       beq backspace           ;if it was a backspace, branch to the backspace routine
+       
+       
+       
+       cmpb #$39               ;accumulator b - $39    check if input is a number
+       bgt input_loop         ;if input is not a number, go to the top of the loop
+       
+       stab a, y               ;load the contents of accumulator b into buffer
+       jsr OUTCHAR             ;display the input charater on the LCD
+       
+       inc COUNT               ;increment count
+       bra input_loop         ;branch back to the top of the loop
+       
+       
+  backspace:
+ 
+       jsr GETADDR                   ;get current position of LCR
+       deca                          ;decrement one
+       jsr SETADDR                   ;set address to new position
+       ldx #BACKSPACE                ;
+       jsr OUTSTRING                 ;output a blank character
+       jsr GETADDR                   ;get current position of LCR
+       deca                          ;decrement one
+       jsr SETADDR                   ;set address to new position
+       dec COUNT                     ;reset the value of count
+     
+       bra input_loop               ;branch back to the top of the loop
+ 
+       
+   input_exit:
+ 
+       ldaa #$00
+       ldx #RESPONSE      ;load message into x
+       jsr OUTSTRING_AT     ;display message
+       rts    ;return to main 
+  
+  ;-------------------------------------------------------------------------------------
+
+convert:
+
+        clrw RESULT              ;sets RESULT to zero
+        clr TMP  
+                       ;sets TMP to zero
+                      
+ ldx #BUFFER            ;load index register x with the contents of BUFFER                       
+                       
+                       
+convert_loop:
+       
+;check status of loop
+       
+        ldaa COUNT             ;load accumulator a with the value of COUNT
+        beq convert_exit       ;if count is 0 then we are done and exit the loop
+       
+;multiply current result value by 10
+
+        ldy RESULT             ;load index register y with the contents of RESULT
+        ldd #$000A             ;load accumulator d with the hex value for 10
+        emul                   ;multiply the contents of d with the contents of y
+        std RESULT             ;store the contents of accumulator d in result
+       
+;get the next number to be added
+
+  
+        ldaa TMP               ;load index register A with the position value TMP
+        ldab a,x               ;load accumulator b with the contents of buffer at position a
+        subb #$30              ;subtract #$30 to get the digital value of the numnber
+               
+;add the number to the multiplied result value and store the result
+ 
+        clra                   ;clear the contents of accumulator a
+        addd RESULT            ;add the contents of accumulator d to result
+        std RESULT             ;store the contents of accumulator d in result
+        inc TMP                ;increment so the next position can be reached
+        dec COUNT              ;decrement count to keep tack of the loop
+        bra convert_loop       ;loop back to the top  
+ 
+ convert_exit:
+     
+        rts ; return to main
+
+        
+;-------------------Cooperative Fixed Messaging-------------------------------------------;        
+
+PUTCHAR1:    
+          stx DPTR
+          jsr SETADDR
+          clr FIRSTCH
+          
+PUTCHAR:          
+          ldx DPTR
+          ldab 0,x
+          beq mess_exit
+          incw DPTR
+          jsr OUTCHAR
+          rts
+
+
+mess_exit:
+
+            movb #$01, t3state
+            rts
+
 
 
 ;/------------------------------------------------------------------------------------\
