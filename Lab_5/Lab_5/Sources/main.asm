7a3a4a8c442c2ce84fec7afdca465dd833b1d275
Aiden-Taylor <57740540+Aiden-Taylor@users.noreply.github.com>
2023-12-04 17:00:13 -0800

    Add files via upload

---
+++ Lab_5/Lab_5/Sources/main.asm
@@ -0,0 +1,112 @@
+;**************************************************************************************
+;* Blank Project Main [includes LibV2.2]                                              *
+;**************************************************************************************
+;* Summary:                                                                           *
+;*   -                                                                                *
+;*                                                                                    *
+;* Author: YOUR NAME                                                                  *
+;*   Cal Poly University                                                              *
+;*   Spring 2022                                                                      *
+;*                                                                                    *
+;* Revision History:                                                                  *
+;*   -                                                                                *
+;*                                                                                    *
+;* ToDo:                                                                              *
+;*   -                                                                                *
+;**************************************************************************************
+
+;/------------------------------------------------------------------------------------\
+;| Include all associated files                                                       |
+;\------------------------------------------------------------------------------------/
+; The following are external files to be included during assembly
+
+
+;/------------------------------------------------------------------------------------\
+;| External Definitions                                                               |
+;\------------------------------------------------------------------------------------/
+; All labels that are referenced by the linker need an external definition
+
+              XDEF  main
+              RUN:        DS.B 1  ;boolean indicatiing controller is running
+              CL:         DS.B 1  ;boolean for closed-loop active
+              V_ref:      DS.W 1  ;reference velocity
+              Theta_OLD:  DS.W 1  ;previous encoder reading
+              KP:         DS.W 1  ;proportional gain [1024*KP]
+              KI:         DS.W 1  ;integral gain [1024*KI]
+              UPDATE_FLG1:DS.B 1  ;Boolean for display update for line one
+
+;/------------------------------------------------------------------------------------\
+;| External References                                                                |
+;\------------------------------------------------------------------------------------/
+; All labels from other files must have an external reference
+
+              XREF  ENABLE_MOTOR, DISABLE_MOTOR
+              XREF  STARTUP_MOTOR, UPDATE_MOTOR, CURRENT_MOTOR
+              XREF  STARTUP_PWM, STARTUP_ATD0, STARTUP_ATD1
+              XREF  OUTDACA, OUTDACB
+              XREF  STARTUP_ENCODER, READ_ENCODER
+              XREF  INITLCD, SETADDR, GETADDR, CURSOR_ON, CURSOR_OFF, DISP_OFF
+              XREF  OUTCHAR, OUTCHAR_AT, OUTSTRING, OUTSTRING_AT
+              XREF  INITKEY, LKEY_FLG, GETCHAR
+              XREF  LCDTEMPLATE, UPDATELCD_L1, UPDATELCD_L2
+              XREF  LVREF_BUF, LVACT_BUF, LERR_BUF,LEFF_BUF, LKP_BUF, LKI_BUF
+              XREF  Entry, ISR_KEYPAD
+              
+              V_act_DISP: DS.W 1  ;actual velocity value
+              ERR_DISP:   DS.W 1  ;error display value
+              EFF_DISP:   DS.W 1  ;effor display vlaue
+              INTERVAL:   DS.W 1  ;number of tick s between interrupts
+              IFENTRY:    DS.W 1  ;LAB 5 INTERFACE subroutine
+            
+;/------------------------------------------------------------------------------------\
+;| Assembler Equates                                                                  |
+;\------------------------------------------------------------------------------------/
+; Constant values can be equated here
+
+
+
+;/------------------------------------------------------------------------------------\
+;| Variables in RAM                                                                   |
+;\------------------------------------------------------------------------------------/
+; The following variables are located in unpaged ram
+
+DEFAULT_RAM:  SECTION
+
+
+
+;/------------------------------------------------------------------------------------\
+;|  Main Program Code                                                                 |
+;\------------------------------------------------------------------------------------/
+; Your code goes here
+
+MyCode:       SECTION
+
+
+main:   jsr IFENTRY
+        
+spin:   bra   spin                     ; endless horizontal loop
+
+
+;/------------------------------------------------------------------------------------\
+;| Subroutines                                                                        |
+;\------------------------------------------------------------------------------------/
+; General purpose subroutines go here
+
+TC0ISR: ;put shit here
+
+;/------------------------------------------------------------------------------------\
+;| ASCII Messages and Constant Data                                                   |
+;\------------------------------------------------------------------------------------/
+; Any constants can be defined here
+
+
+;/------------------------------------------------------------------------------------\
+;| Vectors                                                                            |
+;\------------------------------------------------------------------------------------/
+; Add interrupt and reset vectors here
+
+        ORG   $FFFE                    ; reset vector address
+        DC.W  Entry
+        
+        ORG   $FFEE
+        DC.W  TC0ISR
